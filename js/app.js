d078a38608860e274cf8e381ba00ced6e25d211a
Cem <cemarguvanli@gmail.com>
2015-05-12 11:40:08 +0300

    add element

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-	$('#storeme').submit(function(event) {
+	/*$('#storeme').submit(function() {
 
 		// Grab values for from
 		var name = $('input[name="name"]').val();
@@ -7,4 +7,47 @@ $(document).ready(function() {
 		// Save it
 		localStorage.setItem("person", name);
 	});
+
+	// Show the info
+	$('#showme').html(localStorage.getItem("person"));
+
+	// Remove item
+	$('#removeme').on('click', function() {
+		localStorage.removeItem("person");
+	});*/
+
+
+
+	$('#storeme').submit(function() {
+
+		// Grab values for from
+		var name = $('input[name="name"]').val();
+		var age = $('input[name="age"]').val();
+		var gender = $('input[name="gender"]:checked').val();
+
+		// Save it
+		localStorage.setItem("person", name);
+		localStorage.setItem("age", age);
+		localStorage.setItem("gender", gender);
+	});
+
+	// Show the info
+	var output = [];
+	output.push(localStorage.getItem("person"));
+	output.push(localStorage.getItem("age"));
+	output.push(localStorage.getItem("gender"));
+
+
+
+	$('#showme').html(output[0] + ' ' + output[1] + ' ' + output[2]);
+
+
+
+
+	// Remove item
+	$('#removeme').on('click', function() {
+		localStorage.removeItem("person");
+	});
+
+
 });
